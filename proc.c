9e54bebaf226ab0eb170b4c546623bd9a5c90617
wxy615 <3216058470@qq.com>
2026-05-05 18:33:00 +0800

    Task 2: observed scheduler switching to pid 2

--- proc.c
+++ proc.c
@@ -342,6 +342,7 @@ scheduler(void)
       c->proc = p;
       switchuvm(p);
       p->state = RUNNING;
+      cprintf("[SCHED] switch to pid=%d\n", p->pid);
 
       swtch(&(c->scheduler), p->context);
       switchkvm();
